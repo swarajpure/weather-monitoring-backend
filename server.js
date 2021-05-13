c1941f662df8a7dd0ad5adecb42503a7e1fd9687
Swaraj <swarajrajpure@gmail.com>
2021-05-13 12:00:04 +0530

    basic routing and cors

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const weatherRouter = require('./routes/weather');
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/weather', weatherRouter);
 
 const PORT = process.env.PORT || 4000;
 
